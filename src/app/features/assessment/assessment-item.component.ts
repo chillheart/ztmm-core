d199fc590bd9dc129fcde02857042b6de1018e1d
Cameron McCormick <14103370+chillheart@users.noreply.github.com>
2025-06-24 22:55:57 -0500

    feat: Enable technology descriptions by default in assessment components (#33)

--- src/app/features/assessment/assessment-item.component.ts
+++ src/app/features/assessment/assessment-item.component.ts
@@ -19,7 +19,7 @@ export class AssessmentItemComponent implements OnInit, OnChanges {
   @Input() functionCapabilities: FunctionCapability[] = [];
   @Input() maturityStages: MaturityStage[] = [];
   @Input() isAutoSaving = false;
-  @Input() showDescription = false;
+  @Input() showDescription = true;
 
   @Output() statusChange = new EventEmitter<AssessmentStatus | null>();
   @Output() notesChange = new EventEmitter<string>();
